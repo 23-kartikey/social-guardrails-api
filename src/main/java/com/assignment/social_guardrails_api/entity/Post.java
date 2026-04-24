dcca87d08c9685f3326dfe01d9f87886e9f6b4d7
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-25 03:14:46 +0530

    refactor: remove AuthorType attribute from this

--- src/main/java/com/assignment/social_guardrails_api/entity/Post.java
+++ src/main/java/com/assignment/social_guardrails_api/entity/Post.java
@@ -6,34 +6,26 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @Setter
 @Entity
 @Table(name="posts")
-public class Post {
+public class Post{
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable=false)
-    private AuthorType authorType;
 
     @Column(nullable=false)
     private Long authorId;
