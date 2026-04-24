5c0ad59e157ae32b08069edeaedea269b218e7d3
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-24 05:48:21 +0530

    chore: create User entity

---
+++ src/main/java/com/assignment/social_guardrails_api/entity/User.java
@@ -0,0 +1,30 @@
+package com.assignment.social_guardrails_api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    private boolean is_premium;
+}
