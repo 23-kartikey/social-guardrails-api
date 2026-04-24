229f24c0f53645d57960222b1e868e1a4a36c578
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-24 05:35:29 +0530

    first commit: initialize project with dependencies

---
+++ src/main/java/com/assignment/social_guardrails_api/SocialGuardrailsApiApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.social_guardrails_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialGuardrailsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialGuardrailsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/social_guardrails_api/SocialGuardrailsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.social_guardrails_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialGuardrailsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
