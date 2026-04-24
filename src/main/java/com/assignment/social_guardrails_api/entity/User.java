37b6676e7067bb2296b100b6810ff50c1183ec9c
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-25 03:13:59 +0530

    feat: implemented inheritanc from Author

--- src/main/java/com/assignment/social_guardrails_api/entity/User.java
+++ src/main/java/com/assignment/social_guardrails_api/entity/User.java
@@ -3,27 +3,18 @@ package com.assignment.social_guardrails_api.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
 @Table(name="users")
-public class User {
-    
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
+public class User extends Author{
 
     @NotBlank
     @Column(nullable=false, unique=true)
