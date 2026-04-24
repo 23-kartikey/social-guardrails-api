066afa0a4014f34b9cce8acb49ef5215d3270144
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-24 07:31:16 +0530

    chore: add proper validation to User entity

--- src/main/java/com/assignment/social_guardrails_api/entity/User.java
+++ src/main/java/com/assignment/social_guardrails_api/entity/User.java
@@ -1,11 +1,12 @@
 package com.assignment.social_guardrails_api.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,14 +17,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name="users")
+@Table(name="user")
 public class User {
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+    @NotBlank
+    @Column(nullable=false, unique=true)
     private String username;
 
     private boolean isPremium;
