5d88eb17aa6b685c1343310d99a565d7ee7f810c
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-25 22:49:34 +0530

    feat: add methods for checking if the bot can reply to a post and for checking bot cooldown limits

--- src/main/java/com/assignment/social_guardrails_api/service/BotService.java
+++ src/main/java/com/assignment/social_guardrails_api/service/BotService.java
@@ -1,19 +1,32 @@
 package com.assignment.social_guardrails_api.service;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.assignment.social_guardrails_api.dto.BotRequest;
 import com.assignment.social_guardrails_api.dto.BotResponse;
 import com.assignment.social_guardrails_api.entity.Bot;
+import com.assignment.social_guardrails_api.entity.Comment;
+import com.assignment.social_guardrails_api.entity.Post;
+import com.assignment.social_guardrails_api.exception.BotCommentLimitException;
+import com.assignment.social_guardrails_api.exception.PostNotFoundException;
+import com.assignment.social_guardrails_api.repository.AuthorRepository;
 import com.assignment.social_guardrails_api.repository.BotRepository;
+import com.assignment.social_guardrails_api.repository.PostRepository;
 
 @Service
 public class BotService {
     
     private BotRepository repo;
+    private PostRepository postRepo;
+    private RedisTemplate redisTemplate;
 
-    public BotService(BotRepository repo){
+    public BotService(BotRepository repo, RedisTemplate redisTemplate, PostRepository postRepo){
         this.repo=repo;
+        this.redisTemplate=redisTemplate;
+        this.postRepo=postRepo;
     }
 
     public BotResponse createBot(BotRequest bot){
@@ -30,6 +43,24 @@ public class BotService {
         bot.setName(req.getName());
         bot.setPersonaDescription(req.getPersonaDescription());
         return bot;
+    }
+
+    //redis methods
+
+    public boolean canBotReply(Long postId){
+        String key="post:"+postId+":bot_count";
+        Long count=redisTemplate.opsForValue().increment(key);
+        return count<=100;
+    }
+
+    public boolean checkCooldown(Long botId, Post post){
+        Long userId=post.getAuthorId();
+        String key="cooldown:bot_"+botId+":user_"+userId;
+        if(redisTemplate.hasKey(key)){
+            throw new BotCommentLimitException();
+        }
+        redisTemplate.opsForValue().set(key, "1", 10, TimeUnit.MINUTES);
+        return true;
     }
 
 }
