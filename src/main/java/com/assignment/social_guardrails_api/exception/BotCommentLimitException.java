610501112be5a4e8ea84ef7689c46c16bb4dbb41
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-25 22:51:54 +0530

    feat: create custom exception to return 429 bad request when bot reply limit is reached

---
+++ src/main/java/com/assignment/social_guardrails_api/exception/BotCommentLimitException.java
@@ -0,0 +1,9 @@
+package com.assignment.social_guardrails_api.exception;
+
+public class BotCommentLimitException extends RuntimeException{
+    
+    public BotCommentLimitException(){
+        super();    
+    }
+
+}
