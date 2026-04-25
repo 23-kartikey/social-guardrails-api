50884e2d1bc280d74fa1f98b217cb5fc17a2192f
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-25 23:47:45 +0530

    feat: applied redis checking for bot reply count and botService methods

--- src/main/java/com/assignment/social_guardrails_api/service/CommentService.java
+++ src/main/java/com/assignment/social_guardrails_api/service/CommentService.java
@@ -1,13 +1,15 @@
 package com.assignment.social_guardrails_api.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.assignment.social_guardrails_api.dto.CommentRequest;
 import com.assignment.social_guardrails_api.dto.CommentResponse;
 import com.assignment.social_guardrails_api.entity.Comment;
 import com.assignment.social_guardrails_api.entity.Post;
+import com.assignment.social_guardrails_api.entity.User;
 import com.assignment.social_guardrails_api.exception.PostNotFoundException;
-import com.assignment.social_guardrails_api.repository.BotRepository;
 import com.assignment.social_guardrails_api.repository.CommentRepository;
 import com.assignment.social_guardrails_api.repository.PostRepository;
 import com.assignment.social_guardrails_api.repository.UserRepository;
@@ -15,32 +17,37 @@ import com.assignment.social_guardrails_api.repository.UserRepository;
 @Service
 public class CommentService {
     
+    private static final Logger logger =LoggerFactory.getLogger(CommentService.class);
+
     private final CommentRepository commRepo;
     private final PostRepository postRepo;
     private final UserRepository userRepo;
-    private final BotRepository botRepo;
     private final BotService botService;
     private final ViralityService viralityService;
+    private final UserService userService;
 
-    public CommentService(CommentRepository commRepo, PostRepository postRepo, ViralityService viralityService, BotRepository botRepo, UserRepository userRepo, BotService botService){
+    public CommentService(CommentRepository commRepo, PostRepository postRepo, ViralityService viralityService, UserRepository userRepo, BotService botService, UserService userService){
         this.commRepo=commRepo;
         this.postRepo=postRepo;
-        this.botRepo=botRepo;
         this.userRepo=userRepo;
         this.botService=botService;
         this.viralityService=viralityService;
+        this.userService=userService;
     }
 
     public CommentResponse createComment(CommentRequest req, Long postId){
         Comment comment=toComment(req, postId);
         if(userRepo.existsById(req.getAuthorId())){
+            User user=userRepo.findById(req.getAuthorId()).get();
             viralityService.increaseScore(postId, 50);
             commRepo.save(comment);
+            logger.info(user.getUsername()+" replied to your post");
         }
         else{
             if(botService.canBotReply(postId) && botService.checkCooldown(comment.getAuthorId(), comment.getPost())){
                 viralityService.increaseScore(postId, 1);
                 commRepo.save(comment);
+                userService.handleBotNotification(comment.getPost().getAuthorId(), "Bot "+ comment.getAuthorId()+" replied to your post");
             }
         }
         return toCommentResponse(comment);
