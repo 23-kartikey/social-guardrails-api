ad39851e49de3b663f9a652d8447892dfef4161a
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-25 22:50:18 +0530

    feat: add functionality for incresing virality score and check if the comment is created by a bot or a user

--- src/main/java/com/assignment/social_guardrails_api/service/CommentService.java
+++ src/main/java/com/assignment/social_guardrails_api/service/CommentService.java
@@ -7,22 +7,43 @@ import com.assignment.social_guardrails_api.dto.CommentResponse;
 import com.assignment.social_guardrails_api.entity.Comment;
 import com.assignment.social_guardrails_api.entity.Post;
 import com.assignment.social_guardrails_api.exception.PostNotFoundException;
+import com.assignment.social_guardrails_api.repository.BotRepository;
 import com.assignment.social_guardrails_api.repository.CommentRepository;
 import com.assignment.social_guardrails_api.repository.PostRepository;
+import com.assignment.social_guardrails_api.repository.UserRepository;
 
 @Service
 public class CommentService {
     
     private final CommentRepository commRepo;
     private final PostRepository postRepo;
+    private final UserRepository userRepo;
+    private final BotRepository botRepo;
+    private final BotService botService;
+    private final ViralityService viralityService;
 
-    public CommentService(CommentRepository commRepo, PostRepository postRepo){
+    public CommentService(CommentRepository commRepo, PostRepository postRepo, ViralityService viralityService, BotRepository botRepo, UserRepository userRepo, BotService botService){
         this.commRepo=commRepo;
         this.postRepo=postRepo;
+        this.botRepo=botRepo;
+        this.userRepo=userRepo;
+        this.botService=botService;
+        this.viralityService=viralityService;
     }
 
     public CommentResponse createComment(CommentRequest req, Long postId){
-        return toCommentResponse(commRepo.save(toComment(req, postId)));
+        Comment comment=toComment(req, postId);
+        if(userRepo.existsById(req.getAuthorId())){
+            viralityService.increaseScore(postId, 50);
+            commRepo.save(comment);
+        }
+        else{
+            if(botService.canBotReply(postId) && botService.checkCooldown(comment.getAuthorId(), comment.getPost())){
+                viralityService.increaseScore(postId, 1);
+                commRepo.save(comment);
+            }
+        }
+        return toCommentResponse(comment);
     }
 
     private CommentResponse toCommentResponse(Comment comment){
