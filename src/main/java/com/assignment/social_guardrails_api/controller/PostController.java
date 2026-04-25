a2a1494d391948460895a9de4ac1f1b3c4d19b7c
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-25 22:44:20 +0530

    feat: add method for posting likes

--- src/main/java/com/assignment/social_guardrails_api/controller/PostController.java
+++ src/main/java/com/assignment/social_guardrails_api/controller/PostController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.assignment.social_guardrails_api.dto.CommentRequest;
@@ -12,6 +13,7 @@ import com.assignment.social_guardrails_api.dto.CommentResponse;
 import com.assignment.social_guardrails_api.dto.PostRequest;
 import com.assignment.social_guardrails_api.dto.PostResponse;
 import com.assignment.social_guardrails_api.service.CommentService;
+import com.assignment.social_guardrails_api.service.LikeService;
 import com.assignment.social_guardrails_api.service.PostService;
 
 @RestController
@@ -20,20 +22,27 @@ public class PostController {
 
     private final PostService postService;
     private final CommentService commService;
+    private final LikeService likeService;
 
-    public PostController(PostService postService, CommentService commService){
+    public PostController(PostService postService, CommentService commService, LikeService likeService){
         this.postService=postService;
         this.commService=commService;
+        this.likeService=likeService;
     }
     
     @PostMapping
-    public ResponseEntity<PostResponse> newPost(@RequestBody PostRequest post){
-        return ResponseEntity.ok(postService.newPost(post));
+    public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest post){
+        return ResponseEntity.ok(postService.createPost(post));
     }
 
     @PostMapping("/{postId}/comments")
-    public ResponseEntity<CommentResponse> newComment(@PathVariable Long postId, @RequestBody CommentRequest comment){
+    public ResponseEntity<CommentResponse> createComment(@PathVariable Long postId, @RequestBody CommentRequest comment){
         return ResponseEntity.ok(commService.createComment(comment, postId));
     }
+
+    @PostMapping("/{postId}/like")
+        public ResponseEntity<?> createLike(@PathVariable Long postId, @RequestParam Long userId){
+            return ResponseEntity.status(201).body(likeService.createLike(postId, userId));
+        }
     
 }
