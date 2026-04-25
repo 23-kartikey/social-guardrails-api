5d31183067be9612cfb36ae85893fc43e916c0d7
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-25 22:45:05 +0530

    feat: add handlers for like already exists and bot comment limit reached

--- src/main/java/com/assignment/social_guardrails_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/assignment/social_guardrails_api/exception/GlobalExceptionHandler.java
@@ -18,4 +18,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    @ExceptionHandler(AlreadyLikedException.class)
+    public ResponseEntity<String> handleAlreadyLikedException(AlreadyLikedException ex){
+        return ResponseEntity.status(409).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BotCommentLimitException.class)
+    public ResponseEntity<Void> handleBotCommentLimitException(BotCommentLimitException ex){
+        return ResponseEntity.status(429).build();
+    }
+
 }
