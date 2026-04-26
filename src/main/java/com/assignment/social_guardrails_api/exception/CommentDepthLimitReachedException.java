3a4d133adfba95c482490ea9d07ce686b108837e
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-26 09:04:47 +0530

    feat: custom exception to show comment depth is reached

---
+++ src/main/java/com/assignment/social_guardrails_api/exception/CommentDepthLimitReachedException.java
@@ -0,0 +1,9 @@
+package com.assignment.social_guardrails_api.exception;
+
+public class CommentDepthLimitReachedException extends RuntimeException{
+
+    public CommentDepthLimitReachedException(){
+        super();
+    }
+    
+}
