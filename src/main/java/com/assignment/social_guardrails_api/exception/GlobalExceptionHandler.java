d7b579b46ca80a63e39f672d739cd6c402a5e380
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-26 09:01:40 +0530

    feat: updated to have Comment limit of depth and runtime

--- src/main/java/com/assignment/social_guardrails_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/assignment/social_guardrails_api/exception/GlobalExceptionHandler.java
@@ -33,4 +33,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
+    @ExceptionHandler(CommentDepthLimitReachedException.class)
+    public ResponseEntity<?> handleCommentDepthLimitReachedException(CommentDepthLimitReachedException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Comment Depth of 20 Reached: "+ex.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(RuntimeException ex){
+        return ResponseEntity.status(500).body(ex.getMessage());
+    }
 }
