68e3d02e3b9dc8119b8c074df9d57e1845cd08ac
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-26 07:34:06 +0530

    feat: added validation

--- src/main/java/com/assignment/social_guardrails_api/dto/PostRequest.java
+++ src/main/java/com/assignment/social_guardrails_api/dto/PostRequest.java
@@ -1,5 +1,6 @@
 package com.assignment.social_guardrails_api.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 public class PostRequest {
     
+    @NotNull(message="AuthorId can't be empty")
     private Long authorId;
     private String content;
 
