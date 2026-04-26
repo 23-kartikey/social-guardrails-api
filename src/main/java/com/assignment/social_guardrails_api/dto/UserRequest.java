528c141b9f7565f47596cc35c25f2dbf41b92abe
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-26 07:33:50 +0530

    feat: added validation

--- src/main/java/com/assignment/social_guardrails_api/dto/UserRequest.java
+++ src/main/java/com/assignment/social_guardrails_api/dto/UserRequest.java
@@ -1,5 +1,6 @@
 package com.assignment.social_guardrails_api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 public class UserRequest {
     
+    @NotBlank(message="Username can't be blank")
     private String username;
     
     private boolean isPremium;
